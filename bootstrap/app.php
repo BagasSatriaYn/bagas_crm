6e66a65a1fe4607833374b5ccd79b90bb606f7d7
BagasSatriaYn <duhjvdedhvhjufahfy@gmail.com>
2026-05-08 16:22:32 +0700

    Mixed Content Eror

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\EnsureRole::class,
         ]);
+
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
